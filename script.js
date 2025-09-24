3426642bf14b494522c129903d096e1c15c170be
Sandra Hagevall <sandrahagevall@hotmail.com>
2025-09-24 16:03:07 +0200

    add media query and const to JS

---
+++ script.js
@@ -0,0 +1,4 @@
+const box1 = document.getElementById("box1")
+const filterButtons = document.querySelector("btn-filter")
+const sortButtons = document.querySelector("btn-sort")
+const randomButtons = document.getElementById("randomBtn")
